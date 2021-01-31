e1436cd457085ceb1438456a952756cd02b5d250
Buleskyssr <1684298875@qq.com>
2021-01-31 17:07:15 +0800

    Site updated: 2021-01-31 17:07:15

---
+++ dist/music.js
@@ -0,0 +1,10 @@
+const ap = new APlayer({
+    container: document.getElementById('player'),
+    mini: true,
+    audio: [{
+        name: 'name',
+        artist: 'artist',
+        url: '//music.163.com/outchain/player?type=2&id=1500788926&auto=1&height=66',
+        cover: 'cover.jpg',
+    }]
+});
